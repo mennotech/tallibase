c7565579cc2028b9215de2d490c6966def9e8b72
Roland Penner <roland@mennotech.ca>
2024-08-01 22:33:32 -0500

    Moved Tallibase-AD-Collector to separate repo

--- scripts/win/Tallibase-AD-Collector/Save-Password.ps1
+++
@@ -1,5 +0,0 @@
-$Username = Read-Host "Enter Username (cannot contain colon ':')"
-$Password = Read-Host "Enter Password" -AsSecureString |  ConvertFrom-SecureString
-
-
-"$UserName`n$Password" | Out-File Encrypted-Password.txt -Force
